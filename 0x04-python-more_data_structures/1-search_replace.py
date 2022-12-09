e5e4c5e648b81e0fa7fbad0c8fe64b2f4363cbf8
SegoML <masegoletsoko8@gmail.com>
2022-12-09 12:10:46 -0800

    c

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-# 1-search_replace.py
-# Brennan D Baraban <375@holbertonschool.com
-
-
 def search_replace(my_list, search, replace):
-    """Replace all occurrences of an element by another in a new list."""
-    new_list = my_list[:]
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-    return (new_list)
+    new_list = []
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+    return new_list
