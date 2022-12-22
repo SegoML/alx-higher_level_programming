0ffb10e49a166ca8d846b3d6aa0951891b135314
SegoML <masegoletsoko8@gmail.com>
2022-12-21 16:55:10 -0800

    again

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    a = 0
-    i = 0
-    while a < x:
-        try:
-            print(my_list[a], end="")
-            i = i + 1
-        except:
-            pass
-        a = a + 1
-    print("")
-    return i
+    try:
+        count = 0
+        while count < x:
+            print("{}".format(my_list[count]), end='')
+            count += 1
+        print()
+        return count
+    except IndexError:
+        print()
+        return count
+        pass
